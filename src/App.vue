d02b82069a60c889fecd7e52cca25b0312010d0a
이호인 <lovi0714@kyonggi.ac.kr>
2023-07-03 15:40:20 +0900

    변경

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<!-- .vue 파일은 3가지로 이루어져 있음 --> 
+
+<!-- html 코드가 들어감 -->
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<!-- javascript 로직이 들어감 -->
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<!-- css 코드가 들어감 -->
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

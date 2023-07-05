e11c3b73ca130a0716338161c63297827c38bff1
이호인 <lovi0714@kyonggi.ac.kr>
2023-07-05 23:37:23 +0900

    20230705
    vue 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,69 @@
 
 <!-- html 코드가 들어감 -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <form 
+      @submit.prevent="onSubmit"
+      class="d-flex"
+    >
+      <div class="flex-grow-1 mr-2">
+        <input 
+          class="form-control" 
+          type="text" 
+          v-model="todo" 
+          placeholder="Type new to-do"
+        >
+      </div>
+      <div>
+        <button 
+          class="btn btn-primary" 
+          type="submit"
+        > 
+          Add
+        </button>
+      </div>
+    </form>
+    <div class="card mt-2">
+      <div class="card-body p-2">
+        {{ todos[0].subject}}
+      </div>
+    </div>
+    <div class="card mt-2">
+      <div class="card-body p-2">
+        {{ todos[1].subject}}
+      </div>
+    </div>
+  </div>
 </template>
 
 <!-- javascript 로직이 들어감 -->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    setup() {
+      const todo = ref('');
+      const todos = ref([
+        {id:1, subject: '휴대폰 사기'},
+        {id:2, subject: '장보기'},
+      ]);
+
+      const onSubmit = () => {
+        todos.value.push({
+          id: Date.now(),
+          subject: todo.value
+        });
+      };
+
+      return {
+        todo,
+        todos,
+        onSubmit,
+      }
+
+    }
   }
-}
 </script>
 <!-- css 코드가 들어감 -->
 <style>

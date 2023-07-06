7c0349378ba04fd6a86b8aad44fed38a190e1019
이호인 <lovi0714@kyonggi.ac.kr>
2023-07-06 23:54:48 +0900

    20230706
    반복문, 조건문, 체크박스, class, style, 삭제

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,64 @@
 
 <!-- html 코드가 들어감 -->
 <template>
+<!-- 
+  v-show 사용, 초기 렌더링 비용이 높음, 토글 비용이 낮음 (토글이 잦은 기능에 적용하면 좋음)
+  <div v-show="toggle">true</div>
+  <div v-show="!toggle">false</div>
+
+  v-if는 렌더링 할 때부터 조건에 맞는 태그만 가져옴, 토글하는데 비용이 많이 든다. (런타임 동안에 거의 안 바뀔 기능에 적용하는 것이 좋음) 
+  <div v-if="toggle">true</div>
+  <div v-else>false</div>
+  <button @click="onToggle">Toggle</button>
+-->
   <div class="container">
     <h2>To-Do List</h2>
-    <form 
-      @submit.prevent="onSubmit"
-      class="d-flex"
-    >
-      <div class="flex-grow-1 mr-2">
-        <input 
-          class="form-control" 
-          type="text" 
-          v-model="todo" 
-          placeholder="Type new to-do"
-        >
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input 
+            class="form-control" 
+            type="text" 
+            v-model="todo" 
+            placeholder="Type new to-do"
+          >
+        </div>
+        <div>
+          <button 
+            class="btn btn-primary" 
+            type="submit"
+          > 
+            Add
+          </button>
+        </div>  
       </div>
-      <div>
-        <button 
-          class="btn btn-primary" 
-          type="submit"
-        > 
-          Add
-        </button>
+      <div v-show="hasError" style="color: red">
+        This field cannot be empty
       </div>
     </form>
-    <div class="card mt-2">
-      <div class="card-body p-2">
-        {{ todos[0].subject}}
-      </div>
+    <div v-if="!todos.length">
+      추가된 Todo가 없습니다.
     </div>
-    <div class="card mt-2">
-      <div class="card-body p-2">
-        {{ todos[1].subject}}
+    
+    <!-- v-for를 사용할 때는 키를 넣어주어야함 (key 바인딩 필요) -->
+    <div 
+      v-for="(todo, index) in todos"
+      :key="todo.id"
+      class="card mt-2"
+    >
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
+          <input class="form-check-input" type="checkbox" v-model="todo.completed">
+          <!-- 
+            style 바인딩 방식 
+          <label class="form-check-label" :style="todo.completed ? todoStyle : {}">{{ todo.subject}}</label>
+          -->
+          <!-- class 바인딩 방식 -->
+          <label class="form-check-label" :class="{ todo: todo.completed}">{{ todo.subject}}</label>
+        </div>
+        <div>
+          <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">Delete</button>
+        </div>
       </div>
     </div>
   </div>
@@ -44,22 +71,46 @@
 
   export default {
     setup() {
+      const toggle = ref(false);
       const todo = ref('');
-      const todos = ref([
-        {id:1, subject: '휴대폰 사기'},
-        {id:2, subject: '장보기'},
-      ]);
+      const todos = ref([]);
+
+      const hasError = ref(false);
+      const todoStyle = {
+        textDecoration: 'line-through',
+        color: 'gray'
+      }
 
       const onSubmit = () => {
+        if (todo.value === ''){
+          hasError.value = true;
+        } else {
         todos.value.push({
           id: Date.now(),
-          subject: todo.value
+          subject: todo.value,
+          completed: false,
         });
+        hasError.value = false;
+        todo.value = '';
+        }
       };
+
+      const onToggle = () => {
+        toggle.value = !toggle.value;
+      }
+
+      const deleteTodo = (index) => {
+          todos.value.splice(index, 1);
+      }
 
       return {
         todo,
         todos,
+        toggle,
+        hasError,
+        todoStyle,
+        deleteTodo,
+        onToggle,
         onSubmit,
       }
 
@@ -68,12 +119,8 @@
 </script>
 <!-- css 코드가 들어감 -->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ .todo {
+  color: gray;
+  text-decoration: line-through;
+ }
 </style>
